82732997b20993cf281f18ff14b12c1f41ff0e04
avaid96 <avaid1996@gmail.com>
2016-05-02 14:50:15 -0500

    writing results to csv

--- scraping.py
+++ scraping.py
@@ -4,7 +4,8 @@ import webbrowser
 import urllib
 import re
 from alchemyapi import AlchemyAPI
-
+import csv
+from datetime import date
 alchemyapi=AlchemyAPI()
 
 def scrapeapage(url):
@@ -15,9 +16,16 @@ def scrapeapage(url):
 
 def getsent(link):
 	myText = alchemyapi.text("url",link)
+	author = alchemyapi.author("url",link)
+	author= author['author'].encode('utf-8')
 	myText=myText['text'].encode('utf-8')
-	response = alchemyapi.sentiment("text", myText)
-	return "Sentiment: ", response
+	sentiment = alchemyapi.sentiment("text", myText)
+	print "Author: ", author
+	print "\n"
+	print "Url: ", link
+	print "\n"
+	print "Sentiment: ", sentiment
+	c.writerow([today,link,author,sentiment['docSentiment']['type'],sentiment['docSentiment']['score']])
 
 def wsjrtrep():
 	mainpage=scrapeapage("http://blogs.wsj.com/chinarealtime/")
@@ -30,9 +38,10 @@ def wsjrtrep():
 		if(links[i]!=None):
 			linklist.append(links[i].group()[1:-1])
 	print linklist #list of the links of the headliners
-	print "Check out Alchemy API from url"
 	for link in linklist:
-		print getsent(link)
+		getsent(link)
 
-
+c=csv.writer(open("SentimentOp.csv","a"),lineterminator='\n')
+c.writerow(["Date","URL","Author","Sentiment Type","Sentiment score"])
+today=date.today()
 wsjrtrep()
