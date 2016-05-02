854d59a4ff0348c37c820e7029f665644c49fbbc
avaid96 <avaid1996@gmail.com>
2016-05-02 14:22:18 -0500

    included alchemy functionality replacing from scratch build

--- scraping.py
+++ scraping.py
@@ -3,12 +3,21 @@ import csv
 import webbrowser
 import urllib
 import re
+from alchemyapi import AlchemyAPI
+
+alchemyapi=AlchemyAPI()
 
 def scrapeapage(url):
 	opener=urllib.FancyURLopener({})
 	openerFile = opener.open(url)
 	htmlFile = openerFile.read()
 	return htmlFile
+
+def getsent(link):
+	myText = alchemyapi.text("url",link)
+	myText=myText['text'].encode('utf-8')
+	response = alchemyapi.sentiment("text", myText)
+	return "Sentiment: ", response
 
 def wsjrtrep():
 	mainpage=scrapeapage("http://blogs.wsj.com/chinarealtime/")
@@ -20,6 +29,10 @@ def wsjrtrep():
 		links[i]=linkreg.search(str(links[i]))
 		if(links[i]!=None):
 			linklist.append(links[i].group()[1:-1])
-	return linklist #list of the links of the headliners
+	print linklist #list of the links of the headliners
+	print "Check out Alchemy API from url"
+	for link in linklist:
+		print getsent(link)
+
 
 wsjrtrep()
